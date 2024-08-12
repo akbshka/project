da8213cb5471cfa5b97c735982e90f4b9c250d86
Ruslan <exemple@gmail.com>
2024-08-12 11:27:59 +0300

    array lessons

--- js/h-w.js
+++ js/h-w.js
@@ -1,57 +1,33 @@
 
-const options = {
-    name: 'test',
-    width: 1024,
-    height: 1024,
-    colors: {
-        border: 'black',
-        bg: 'red',
-    },
-    makeTest: function() {
-        console.log('Test')
-    }
-}
-options.makeTest()
 
-const {border, bg} = options.colors
-console.log(border)
-
-// console.log(Object.keys(options).length)
+const arr = [2, 13, 26, 8, 10];
+arr.sort((a,b) => a - b)
+console.log(arr)
 
 
-// const newObj = {
-//     name: 'Ruslan',
-//     age: 42,
-//     family: {
-//         son: 'Eldar',
-//         doter: 'Esmira',
-//         wife: 'Lilia',
-//     },
-//     national: 'Tatar',
-//     country: 'Russia'
+
+// arr.forEach((item, i, arr) =>  {
+//     console.log(`${i}: ${item} внутрий массива ${arr}`)
+// })
+
+// arr[99] = 0
+// console.log(arr.length)
+// console.log(arr)
+
+
+// arr.pop()
+// arr.push(10)
+// console.log(arr)
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i])
 // }
-// console.log(Object.keys(newObj).length)
 
-
-
-
-
-// console.log(options['colors']['border']);
-
-// delete options.name;
-
-// console.log(options)
-// let counter = 0;
-// for (let key in options) {
-//     if (typeof options[key] === 'object'){
-//         for (let i in options[key]){
-//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`)
-            
-//         }
-//     }else {
-//         console.log(`Свойство ${key} имеет значение ${options[key]}`)
-//         counter++
-//     }
+// for( let value of arr) {
+//     console.log(value)
 // }
-// console.log(counter)
 
+// const str = prompt('', '');
+// const products = str.split(', ')
+// products.sort();
+// console.log(products.join('; '))
